88e2e83310a926f8a5e1150481054abd42cac48a
Alejandro Ramos <aleramos119@gmail.com>
2025-03-03 22:04:17 +0100

    lesson 9

--- api/views.py
+++ api/views.py
@@ -6,6 +6,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 import logging
 
+from django.http import JsonResponse
+
 # Configure logging
 logger = logging.getLogger(__name__)
 
@@ -74,3 +76,17 @@ class CreateRoomView(APIView):
             return Response(RoomSerializer(room).data, status=status.HTTP_201_CREATED)
 
         return Response({'Bad Request': 'Invalid data...'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+class UserInRoom(APIView):
+    def get(self, request, format=None):
+
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create()
+
+        data ={
+            'code': self.request.session.get('room_code')
+        }
+
+        return JsonResponse(data, status=status.HTTP_200_OK)
--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,12 +1,49 @@
 import React, { Component} from "react";
+import { Link, Navigate, Routes, Route } from "react-router-dom";
+
+import {Grid, Button, ButtonGroup, Typography} from '@mui/material';
 
 export default class HomePage extends Component {
     
     constructor(props){
         super(props);
+        this.state = {roomCode: null};
+    }
+
+    async componentDidMount(){
+        fetch('/api/user-in-room')
+        .then((response) => response.json())
+        .then((data) => {
+            this.setState({roomCode: data.code});
+        });
+    }
+
+    renderHomePage(){
+        return (
+            <Grid container spacing={3}>
+                <Grid item xs={12} align="center">
+                    <Typography variant="h4" component="h4">
+                        House Party
+                    </Typography>
+                </Grid>
+                <Grid item xs={12} align="center">
+                    <ButtonGroup disableElevation variant="contained" color="primary">
+                        <Button color="primary" component={Link} to='/join'>Join a Room</Button>
+                        <Button color="secondary" component={Link} to='/create'>Create a Room</Button>
+                    </ButtonGroup>
+                </Grid>
+            </Grid>
+        )
     }
 
     render(){
-        return (<p>This is the Home Page</p>)
+        return (
+            <Routes>
+                <Route 
+                    path="/" 
+                    element={this.state.roomCode ? <Navigate to={`/room/${this.state.roomCode}`} /> : this.renderHomePage()}
+                />
+            </Routes>
+        )
     }
 }

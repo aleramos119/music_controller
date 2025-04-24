b06d90076b0f40f813fc36697fc4a1f0c03c1a92
Alejandro Ramos <aleramos119@gmail.com>
2025-04-24 21:25:19 +0200

    Room page styling done

--- frontend/src/components/Room.js
+++ frontend/src/components/Room.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
+import { Grid, Button, Typography} from "@mui/material";
+
 
 // Create a wrapper component to use hooks
 function RoomWithParams() {
@@ -48,14 +50,41 @@ function Room() {
     }
 
     return (
-        <div>
+        <Grid container spacing={1} direction="column" alignItems="center">
+            <Grid item xs={12}>
+                <Typography variant="h5">Room Code: {roomCode}</Typography>
+            </Grid>
+            <Grid item xs={12}>
+                <Typography variant="h6">Room Details</Typography>
+            </Grid>
+            <Grid item xs={12}>
+                <Typography>Votes to Skip: {roomDetails.votesToSkip}</Typography>
+            </Grid>
+            <Grid item xs={12}>
+                <Typography>Guest Can Pause: {roomDetails.guestCanPause.toString()}</Typography>
+            </Grid>
+            <Grid item xs={12}>
+                <Typography>Host: {roomDetails.isHost.toString()}</Typography>
+            </Grid>
+            <Grid item xs={12}>
+                <Button variant="contained" color="secondary" onClick={() => console.log('Leave Room')}>
+                    Leave Room
+                </Button>
+            </Grid>
+        </Grid>
+
+
+        
+    );
+}
+
+
+{/* <div>
             <h3>Room Code: {roomCode}</h3>
             <h3>Room Details</h3>
             <p>Votes to Skip: {roomDetails.votesToSkip}</p> 
             <p>Guest Can Pause: {roomDetails.guestCanPause.toString()}</p> 
             <p>Host: {roomDetails.isHost.toString()}</p> 
-        </div>
-    );
-}
+        </div> */}
 
 export { Room, RoomWithParams as default };
